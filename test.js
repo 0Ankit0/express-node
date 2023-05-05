085fd8a074b0a69d771a6487866c30cbcd7760f5
ankit <ankit@DESKTOP>
2023-05-05 12:12:17 +0545

    nodemon and mongoose

--- test.js
+++ test.js
@@ -1,1 +1,18 @@
-   console.log("hello world"); 
+   //run npm init to initialize package.json
+   //node filename to run file locally
+   //npm install express to download express
+   //download thunder client extension
+   //npm install mongoose
+   //npm install --save-dev nodemon to download the extension while building and don't run while deploying nodemonitor
+   
+   const express = require("express"); //importing express
+   const httpServer = express();
+   
+
+   httpServer.get("/",(req,res)=>{//request and respond as parameters
+    console.log("new request found");
+    res.send("Hello there")
+   })
+   httpServer.listen(3000,()=>{
+    console.log("listening to port 3000");
+   })
